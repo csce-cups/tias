fc481542e19fa68cf05d372433727e6342479db8
Ben Beauchamp <benbeauchamp7@gmail.com>
2022-02-24 20:06:43 -0600

    Filter bar with hover effect

--- tias/src/Components/Scheduling/SchedulingFilter.tsx
+++ tias/src/Components/Scheduling/SchedulingFilter.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export const SchedulingFilter = () => {
   return (
-	  <div className="row">
-      Filter
+	  <div className="row filter">
+      <div className="static filter element">CSCE:</div>
+      <div className="center filter element">ALL</div>
+      <div className="center filter element">121</div>
+      <div className="center filter element">221</div>
     </div>
   )
 }
