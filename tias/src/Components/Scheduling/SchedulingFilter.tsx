aacbbcfcc1da0e6cb15af4609f9375194eda0c80
Ben Beauchamp <benbeauchamp7@gmail.com>
2022-02-24 23:01:23 -0600

    Added schedule layout grid

--- tias/src/Components/Scheduling/SchedulingFilter.tsx
+++ tias/src/Components/Scheduling/SchedulingFilter.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
 export const SchedulingFilter = () => {
+  let filter_subjects = ['ALL', '121', '221', '312', '313', '314', '315'];
+  let filter_elements = [];
+  
+  for (let i = 0; i < filter_subjects.length; i++) {
+    filter_elements[i] = <div className="center filter element">{filter_subjects[i]}</div>;
+  }
+
   return (
 	  <div className="row filter">
       <div className="static filter element">CSCE:</div>
-      <div className="center filter element">ALL</div>
-      <div className="center filter element">121</div>
-      <div className="center filter element">221</div>
+      {filter_elements}
     </div>
   )
 }
--- tias/src/Components/Scheduling/SchedulingRender.tsx
+++ tias/src/Components/Scheduling/SchedulingRender.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 export const SchedulingRender = () => {
+  let grid = [];
+  for (let i = 0; i < 5*11; i++) {
+    grid.push(<div></div>)
+  }
+
   return (
-	  <div className="render-window">
-      <div>Render</div>
-    </div>
+    <>
+      <div className="render-grid slim">
+        {grid}
+      </div>
+    </>
   )
 }
