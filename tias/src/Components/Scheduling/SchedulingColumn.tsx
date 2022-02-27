76af5a5ad307d890fb62c8e190b50dc69a244fa7
Ben Beauchamp <benbeauchamp7@gmail.com>
2022-02-27 02:14:47 -0600

    New packing algorithm for schedule render, broken for section 3, sorter needs work for section 2

--- tias/src/Components/Scheduling/SchedulingColumn.tsx
+++ tias/src/Components/Scheduling/SchedulingColumn.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { SchedulingBlock } from './SchedulingBlock';
 
+// FOR TEST SCHEDULES //
 const classlength = 50;
 const breaklength = 10;
 
@@ -39,8 +40,9 @@ let B3_long = new Date(B3.getTime() + 1000*60*(classlength*2+breaklength));
 
 let C3 = new Date(B3.getTime() + 1000*60*(classlength+breaklength));
 let C3_short = new Date(C3.getTime() + 1000*60*classlength);
+// ================================= //
 
-const numHours = 12;
+const numHours = 11;
 interface Props {
   end? : boolean
 }
@@ -55,102 +57,6 @@ interface CourseInstance { // Results of a join between course, course_section, 
 
 // Breaks for 3 different lengths in a block
 const d_len = (e: CourseInstance | {course: number, section: number, start: Date, end: Date}) => { return e.end.getTime() - e.start.getTime(); }
-const generateBlocks = (data: CourseInstance[]) => {
-  console.log(data);
-  
-  data.sort((a, b) => {
-    
-    // Sort by start.getTime() time
-    if (a.start.getTime() < b.start.getTime()) return -1;
-    else if (a.start.getTime() > b.start.getTime()) return 1;
-    
-    // Sort by class length
-    else if (d_len(a) > d_len(b)) return -1;
-    else if (d_len(a) < d_len(b)) return 1;
-    
-    // Sort by course number
-    else if (a.course < b.course) return -1;
-    else if (a.course > b.course) return 1;
-
-    // Sort by section number
-    else if (a.section < b.section) return -1;
-    else if (a.section > b.section) return 1;
-    
-    // They're equal
-    else return 0;
-  });
-  
-  console.log(data);
-  console.log("==============================================================")
-
-  let layered_data: any = [];
-  let outer_buffered_data: any = [];
-  let inner_buffered_data: any = [];
-  let i = 0;
-  while (i < data.length) {
-    console.log("BASE", i, data[i], outer_buffered_data, inner_buffered_data)
-    let top_start = data[i].start.getTime();
-    let top_len = d_len(data[i]);
-    let prev_start = top_start;
-    while (i < data.length && d_len(data[i]) === top_len && data[i].start.getTime() === top_start) {
-      console.log("OUTER INSERT", data[i]);
-      outer_buffered_data.push(data[i]);
-      i++;
-      
-      let inner_buffered_data_set: any = [];
-      while (i < data.length && d_len(data[i]) < top_len) {
-        console.log("INNER START", data[i], d_len(data[i]), top_len);
-        while (i < data.length && data[i].start.getTime() === prev_start) {
-          console.log("INNER INSERT", data[i], data[i].start.getTime(), prev_start);
-          inner_buffered_data.push(data[i]);
-          i++
-        }
-        if (inner_buffered_data.length > 0) {
-          prev_start = (i < data.length)? data[i].start.getTime() : prev_start;
-          console.log("INNER PUSH", inner_buffered_data, i-1 < data.length, data[i-1].start.getTime(), prev_start);
-          inner_buffered_data_set.push(inner_buffered_data);
-          inner_buffered_data = [];
-        } else {
-          break;
-        }
-      }
-
-      if (inner_buffered_data_set.length > 0) {
-        console.log("SET PUSH");
-        outer_buffered_data.push(inner_buffered_data_set);
-        inner_buffered_data = [];
-      }
-    }
-    console.log("OUTER PUSH", outer_buffered_data);
-    layered_data.push(outer_buffered_data);
-    outer_buffered_data = [];
-  }
-
-  let a: any = [
-    [
-      {course: 121, section: 1210, start: A, end: A_extralong},
-      [
-        [
-          {course: 121, section: 1211, start: A, end: A_short},
-          {course: 121, section: 1212, start: A, end: A_short},
-          {course: 121, section: 1213, start: A, end: A_short},
-          {course: 121, section: 1214, start: A, end: A_short},
-          {course: 121, section: 1215, start: A, end: A_short},
-        ],
-        [
-          {course: 121, section: 1216, start: new Date(A_short.getTime() + 1000*60*breaklength), end: A_long},
-        ]
-      ]
-    ],
-    [
-      {course: 121, section: 1218, start: A2, end: A2_extralong},
-    ]
-  ];
-  console.log(layered_data);
-  console.log(a);
-  return placeLevel(layered_data);
-  return placeLevel(a);
-}
 
 const time_to_height = (start: Date, end: Date, max: number = numHours*60*60*1000) => {
   return (end.getTime() - start.getTime()) / max * 100;
@@ -159,9 +65,90 @@ const time_to_height = (start: Date, end: Date, max: number = numHours*60*60*100
 const start_time_to_top = (e: any, start: Date, pstart: Date = new Date(0), parent: number = numHours*60*60*1000) => {
   return (start.getTime() - pstart.getTime()) / parent * 100;
 }
+
+const generateBlocks = (data: CourseInstance[]) => {
+  data.sort((a, b) => {
+
+    // Sort by start time
+    if (a.start.getTime() < b.start.getTime()) return -1;
+    else if (a.start.getTime() > b.start.getTime()) return 1;
+    
+    // Sort by course number
+    else if (a.course < b.course) return -1;
+    else if (a.course > b.course) return 1;
+    
+    // Sort by class length
+    else if (d_len(a) > d_len(b)) return -1;
+    else if (d_len(a) < d_len(b)) return 1;
+
+    // Sort by section number
+    else if (a.section < b.section) return -1;
+    else if (a.section > b.section) return 1;
+    
+    // They're equal
+    else return 0;
+  });
+
+  console.log("SORT: ", data);
+
+  let base: any = [];
+  let i = 0;
+  while (i < data.length) {
+
+    let outer: any = [];
+    let max = data[i];
+    while (i < data.length && data[i].start.getTime() >= max.start.getTime() && data[i].end.getTime() <= max.end.getTime()) {
+
+      let inner: any = [];
+      while (i < data.length && d_len(data[i]) < d_len(max)) {
+
+        let temp_arr: any = [];
+        let temp = data[i];
+        while (i < data.length && data[i].start.getTime() === temp.start.getTime() && data[i].end.getTime() === temp.end.getTime()) {
+
+          temp_arr.push(data[i]);
+          i++;
+        }
+        console.log("TEMP: ", temp_arr);
+        if (temp_arr.length > 0) inner.push(temp_arr);
+
+      }
+      console.log("INNER: ", inner)
+      if (inner.length > 0) outer.push(inner);
+      else { outer.push(data[i]); i++;}
+
+    }
+    console.log("OUTER: ", outer);
+    base.push(outer);
+
+  }
+
+  let a: any = [
+    [
+      {course: 121, section: 12, start: A2, end: A2_extralong},
+      [
+        [
+          {course: 121, section: 13, start: A2, end: A2_short},
+        ]
+      ],
+      {course: 221, section: 14, start: A2, end: A2_extralong},
+      [
+        [
+          {course: 313, section: 15, start: A2, end: A2_short},
+        ]
+      ]
+    ]
+  ]
+
+  console.log(a);
+  console.log(base);
+  return placeBlocks(base);
+  return placeBlocks(a);
+
+}
   
-const placeLevel = (blocks: CourseInstance[]) => {
-  const unwrap = (outer: CourseInstance | CourseInstance[][], parent: CourseInstance) => {
+const placeBlocks = (blocks: CourseInstance[]) => {
+  const unravel = (outer: CourseInstance | CourseInstance[][], parent: CourseInstance) => {
     if (Array.isArray(outer)) {
       return (
         <div className="vstack fill">
@@ -202,7 +189,7 @@ const placeLevel = (blocks: CourseInstance[]) => {
           top: `${start_time_to_top(set[0], set[0].start)}%`
         }}>
           { set.map((outer: any) => (
-            unwrap(outer, maxes[idx])
+            unravel(outer, maxes[idx])
           ))}
         </div>
       ))}
@@ -211,20 +198,6 @@ const placeLevel = (blocks: CourseInstance[]) => {
 }
 
 export const SchedulingColumn: FC<Props> = ({end}) => {
-  // let blocks: any = [
-  //   {course: 121, section: 1210, start: A, end: A_extralong},
-  //   {course: 121, section: 1211, start: A, end: A_short},
-  //   {course: 121, section: 1212, start: A, end: A_short},
-  //   {course: 121, section: 1213, start: C, end: C_short},
-  //   {course: 221, section: 2210, start: A, end: A_extralong},
-  //   {course: 313, section: 3130, start: A, end: A_extralong},
-  //   {course: 121_2, section: 11210, start: A2, end: A2_extralong},
-  //   {course: 121_2, section: 11212, start: A2, end: A2_short},
-  //   {course: 121_2, section: 11212, start: A2, end: A2_short},
-  //   {course: 121_2, section: 11213, start: C2, end: C2_short},
-  //   {course: 221_2, section: 12210, start: A2, end: A2_extralong},
-  //   {course: 221_2, section: 12211, start: A2, end: A2_extralong},
-  // ];
   let blocks: any = [
     {course: 121, section: 1, start:  A, end: A_extralong},
     {course: 121, section: 2, start:  A, end: A_short},
@@ -238,7 +211,7 @@ export const SchedulingColumn: FC<Props> = ({end}) => {
     {course: 221, section: 10, start: C, end: C_short},
     {course: 313, section: 11, start: C, end: C_short},
     
-    {course: 121, section: 12, start: A2, end: A2_extralong},
+    {course: 121, section: 12, start: A2, end: A2_extralong}, // Section is incorrect
     {course: 121, section: 13, start: A2, end: A2_short},
     {course: 221, section: 14, start: A2, end: A2_extralong},
     {course: 313, section: 15, start: A2, end: A2_short},
@@ -247,15 +220,14 @@ export const SchedulingColumn: FC<Props> = ({end}) => {
     {course: 314, section: 18, start: A2, end: A2_extralong},
     {course: 314, section: 19, start: A2, end: A2_short},
     {course: 314, section: 20, start: C2, end: C2_short},
-    {course: 121, section: 21, start: C2, end: C2_short},
     
-    {course: 121, section: 21, start: A3, end: A3_short},
-    {course: 121, section: 22, start: A3, end: A3_short},
-    {course: 121, section: 23, start: B3, end: B3_short},
-    {course: 221, section: 24, start: A3, end: A3_extralong},
-    {course: 221, section: 25, start: C3, end: C3_short},
-    {course: 313, section: 26, start: A3, end: A3_extralong},
-    {course: 314, section: 27, start: A3, end: A3_extralong},
+    // {course: 121, section: 21, start: A3, end: A3_short},
+    // {course: 121, section: 22, start: A3, end: A3_short},
+    // {course: 121, section: 23, start: B3, end: B3_short},
+    // {course: 221, section: 24, start: A3, end: A3_extralong}, // Arrested
+    // {course: 221, section: 25, start: C3, end: C3_short},
+    // {course: 313, section: 26, start: A3, end: A3_extralong},
+    // {course: 314, section: 27, start: A3, end: A3_extralong},
   ];
 
 
@@ -268,8 +240,6 @@ export const SchedulingColumn: FC<Props> = ({end}) => {
   for (let i = 0; i < numHours; i++) {
     dividers[i] = <div className="divider"></div>;
   }
-
-  // console.log(blocks);
 
   return (
     <div className="vstack grow-h day" style={style}>
