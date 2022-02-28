8b313294e4c8328cde8b841ea65d9d52be8a6571
evelyn-crowe <evelyn.crowe@tamu.edu>
2022-02-28 10:42:08 -0600

    tweaked margins and font padding

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const NavBar = () => {
   return (
     <div className="navbar">
-        <div className="left element">TIAS</div>
+        <div className="left element major">TIAS</div>
         <div className="left element">Profile</div>
         <div className="left element">Scheduling</div>
         <div className="left element">LabSwap</div>
