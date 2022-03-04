050e3cb56a77095880b8f495dbcc1833e9942057
Ben Beauchamp <benbeauchamp7@gmail.com>
2022-03-03 22:45:19 -0600

    Working away

--- src/Components/Misc/dot.tsx
+++ src/Components/Misc/dot.tsx
@@ -4,8 +4,17 @@ interface Props {
   styles?: any
 }
 
+const emphasizeLinked = () => {
+  let arr = Array.from(document.getElementsByClassName("dot"));
+  // arr.forEach(e => )
+  console.log(arr);
+}
+const deemphasizeLinked = () => {
+  console.log("OUT");
+}
+
 export const Dot: FC<Props> = ({styles}) => {
   return (
-    <div className="dot" style={styles}/>
+    <div className="dot" style={styles} onMouseOver={emphasizeLinked} onMouseOut={deemphasizeLinked}/>
   )
 }
