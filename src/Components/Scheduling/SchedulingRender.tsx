0bd08074d43abc2d5b9ac3f323815184d8db8a70
benbeauchamp7 <43358492+benbeauchamp7@users.noreply.github.com>
2022-03-06 18:03:28 -0600

    Added filters back

--- src/Components/Scheduling/SchedulingRender.tsx
+++ src/Components/Scheduling/SchedulingRender.tsx
@@ -3,15 +3,19 @@ import { SchedulingColumn } from './SchedulingColumn';
 
 import BlockFormer from './BlockFormer';
 
-export const SchedulingRender = () => {
+interface Props {
+  filter: any //int -> bool
+}
+
+export const SchedulingRender: FC<Props> = ({filter}) => {
   return (
     <div className="render-container">
       <div className="render-content">
-        < SchedulingColumn blocks={BlockFormer.samples.M_schedule} />
-        < SchedulingColumn blocks={BlockFormer.samples.TH_shcedule} />
-        < SchedulingColumn blocks={BlockFormer.samples.W_schedule} />
-        < SchedulingColumn blocks={BlockFormer.samples.TH_shcedule} />
-        < SchedulingColumn blocks={BlockFormer.samples.F_schedule} end={true} />
+        < SchedulingColumn blocks={BlockFormer.samples.M_schedule} filter={filter} />
+        < SchedulingColumn blocks={BlockFormer.samples.TH_shcedule} filter={filter} />
+        < SchedulingColumn blocks={BlockFormer.samples.W_schedule} filter={filter} />
+        < SchedulingColumn blocks={BlockFormer.samples.TH_shcedule} filter={filter} />
+        < SchedulingColumn blocks={BlockFormer.samples.F_schedule} filter={filter} end={true} />
       </div>
     </div>
   )
