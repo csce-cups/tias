7188c4c4563869a1ee511ca3a174a883a35eb1f4
pulchroxloom <code@dantebarbieri.dev>
2022-03-18 15:22:25 -0400

    Update fetching person

--- lambda/getSchedule/Driver.java
+++ lambda/getSchedule/Driver.java
@@ -18,11 +18,10 @@ public class Driver {
 
     public static void main(String[] args) {
         // https://jdbc.postgresql.org/documentation/head/index.html
-        String url = "jdbc:postgresql://localhost/test"; // TODO: Set values in Parameter Store
+        String url = "jdbc:postgresql://tias-db-instance.crx9tdpnvcfm.us-east-1.rds.amazonaws.com/tias_db"; // TODO: Set values in Parameter Store
         Properties props = new Properties();
-        props.setProperty("user","username"); // TODO: Set values in Parameter Store
-        props.setProperty("password","secret"); // TODO: Set values in Parameter Store
-        props.setProperty("ssl","true"); // TODO: Set values in Parameter Store
+        props.setProperty("user","cupsaws"); // TODO: Set values in Parameter Store
+        props.setProperty("password","bemyguestputmypasswordtothetest1423!"); // TODO: Set values in Parameter Store
         try {
             conn = DriverManager.getConnection(url, props);
         } catch (Exception e) {
@@ -36,7 +35,14 @@ public class Driver {
         sections = new HashMap<Integer, Section>();
 
         try {
-            getTypeMapping();
+            getCourses();
+            courses.forEach((key, value) -> {
+                System.out.println(key + "\t" + value);
+            });
+            getPeople(new int[]{3, 5});
+            people.forEach((key, value) -> {
+                System.out.println(key + "\t" + value);
+            });
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -50,7 +56,7 @@ public class Driver {
 
     static void getCourses() throws SQLException {
         Statement st = conn.createStatement();
-        ResultSet rs = st.executeQuery("SELECT course_id, department, course_number, course_name FROM courses");
+        ResultSet rs = st.executeQuery("SELECT course_id, department, course_number, course_name FROM course");
         while (rs.next())
         {
             courses.put(rs.getInt("course_id"), new Course(rs.getString("department"), rs.getString("course_number"), rs.getString("course_name")));
@@ -59,46 +65,82 @@ public class Driver {
         st.close();
     }
 
-    static String constructPeopleQuery(int numPeople) {
+    static String constructPersonQuery(String table, int numPeople) {
         StringBuilder sb = new StringBuilder()
-            .append("SELECT")
-            .append("person.person_id, person.first_name, person.last_name, person.peer_teacher, person.teaching_assistant, person.administrator, person.professor,")
-            .append("person_availability.weekday, person_availability.start_time, person_availability.end_time,")
-            .append("section_assignment_preference.section_id, section_assignment_preference.preference,")
-            .append("qualification.course_id, qualification.grade")
-            .append("FROM person")
-            .append("WHERE person.person_id IN (");
-        for (int i = 0; i < numPeople - 1; ++i) {
-            sb.append("?,");
-        }
-        sb.append("?)")
-            .append("LEFT OUTER JOIN person_availability")
-            .append("WHERE person.person_id = person_availability.person_id")
-            .append("LEFT OUTER JOIN section_assignment_preference")
-            .append("WHERE person.person_id = section_assignment_preference.person_id")
-            .append("LEFT OUTER JOIN qualification")
-            .append("WHERE person.person_id = qualification.person_id");
-        return sb.toString();
+            .append("SELECT *")
+            .append("FROM ")
+            .append(table)
+            .append(' ')
+            .append("WHERE person_id IN (");
+            for (int i = 0; i < numPeople - 1; ++i) {
+                sb.append("?, ");
+            }
+            sb.append("?)");
+            return sb.toString();
     }
 
-    static void getPeople(String[] peopleIds) throws SQLException {
-        PreparedStatement st = conn.prepareStatement(constructPeopleQuery(peopleIds.length));
+    static void getPeople(int[] peopleIds) throws SQLException {
+        System.out.println(constructPersonQuery("person", peopleIds.length));
+        PreparedStatement st = conn.prepareStatement(constructPersonQuery("person", peopleIds.length));
 
         for (int i = 0; i < peopleIds.length; ++i) {
-            st.setString(i + 1, peopleIds[0]);
+            st.setInt(i + 1, peopleIds[i]);
         }
 
         ResultSet rs = st.executeQuery();
 
         while (rs.next())
         {
-            Person added = people.putIfAbsent(rs.getInt("person_id"), new Person(rs.getString("first_name"), rs.getString("last_name"), rs.getBoolean("peer_teacher"), rs.getBoolean("teaching_assistant"), rs.getBoolean("administrator"), rs.getBoolean("professor")));
+            people.put(rs.getInt("person_id"), new Person(rs.getString("first_name"), rs.getString("last_name"), rs.getBoolean("peer_teacher"), rs.getBoolean("teaching_assistant"), rs.getBoolean("administrator"), rs.getBoolean("professor")));
+        }
 
-            added.addAvailability(new Availability(rs.getString("weekday"), rs.getTime("start_time"), rs.getTime("end_time")));
+        rs.close();
+        st.close();
 
-            added.addPreference(new Preference(rs.getInt("section_id"), rs.getString("preference")));
+        st = conn.prepareStatement(constructPersonQuery("person_availability", peopleIds.length));
 
-            added.addQualification(new Qualification(rs.getInt("course_id"), (Character) rs.getObject("grade")));
+        for (int i = 0; i < peopleIds.length; ++i) {
+            st.setInt(i + 1, peopleIds[i]);
+        }
+
+        rs = st.executeQuery();
+
+        while (rs.next())
+        {
+            people.get(rs.getInt("person_id")).addAvailability(new Availability(rs.getString("weekday"), rs.getTime("start_time"), rs.getTime("end_time")));
+        }
+
+        rs.close();
+        st.close();
+
+        st = conn.prepareStatement(constructPersonQuery("section_assignment_preference", peopleIds.length));
+
+        for (int i = 0; i < peopleIds.length; ++i) {
+            st.setInt(i + 1, peopleIds[i]);
+        }
+
+        rs = st.executeQuery();
+
+        while (rs.next())
+        {
+            people.get(rs.getInt("person_id")).addPreference(new Preference(rs.getInt("section_id"), rs.getString("preference")));
+        }
+
+        rs.close();
+        st.close();
+
+        st = conn.prepareStatement(constructPersonQuery("qualification", peopleIds.length));
+
+        for (int i = 0; i < peopleIds.length; ++i) {
+            st.setInt(i + 1, peopleIds[i]);
+        }
+
+        rs = st.executeQuery();
+
+        while (rs.next())
+        {
+            String grade = rs.getString("grade");
+            people.get(rs.getInt("person_id")).addQualification(new Qualification(rs.getInt("course_id"), grade != null ? grade.charAt(0) : null));
         }
 
         rs.close();
