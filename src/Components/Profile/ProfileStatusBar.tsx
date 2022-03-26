bbe1755b9d2282ee6e5432c82404f9fc02df304d
Ben Beauchamp <benbeauchamp7@gmail.com>
2022-03-26 13:20:48 -0500

    Cleaned up the status bar above the form

--- src/Components/Profile/ProfileStatusBar.tsx
+++ src/Components/Profile/ProfileStatusBar.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export const ProfileStatusBar = () => {
   return (
-    <div className="hstack"> 
+    <div className="profile-status-bar"> 
       <button className="green button">Upload Schedule</button>
-      <div className="render-container"  style={{margin : "5px"}}>
+      <div className="profile-status">
         <div className="left element">Last Updated: </div>
         <div className="fill element"/>
-        <div className="element"style={{margin : "5px"}}>Jan 1, 1970</div>
+        <div className="element" style={{marginRight: '5px'}}>Jan 1, 1970</div>
       </div>
     </div>
   )
