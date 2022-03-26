faaaa047b0d199a5636dbea2f8444a4689203c1d
Ben Beauchamp <benbeauchamp7@gmail.com>
2022-03-26 15:54:36 -0500

    Removed unused App.tsx in Component folder

--- src/pages/Scheduling.tsx
+++ src/pages/Scheduling.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { EmployeeList } from '../Components/EmployeeList/EmployeeList';
 import { SchedulingWindow } from '../Components/Scheduling/SchedulingWindow';
-import './../Components/common.scss';
+import { APIContext } from '../Components/APIContext'
+import '../Components/common.scss';
 
 export const Scheduling = () => {
   return (
-    <div className="App">
-      <div className="app-body">
-      < EmployeeList />
-      < SchedulingWindow />
-      </div>
-  </div>
+    <div className="app-body">
+      < APIContext test={true}>
+        < EmployeeList />
+        < SchedulingWindow />
+      </APIContext>
+    </div>
   );
 }
 
