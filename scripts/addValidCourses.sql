ac947cd3c7e71b874658fef73f6da06be771ed94
pulchroxloom <code@dantebarbieri.dev>
2022-03-26 21:36:09 -0500

    Update sql to return preference

--- scripts/addValidCourses.sql
+++ scripts/addValidCourses.sql
@@ -1,6 +1,7 @@
 CREATE FUNCTION public."ValidCourses"(IN "P_person_id" integer)
     RETURNS table (course_id course.course_id%TYPE, department course.department%TYPE, course_number course.course_number%TYPE, course_name course.course_name%TYPE,
 	section_id course_section.section_id%TYPE, section_number course_section.section_number%TYPE, placeholder_professor_name course_section.placeholder_professor_name%TYPE,
+    preference section_assignment_preference.preference%TYPE,
 	weekday section_meeting.weekday%TYPE, start_time section_meeting.start_time%TYPE, end_time section_meeting.end_time%TYPE, place section_meeting.place%TYPE)
     LANGUAGE 'sql'
     
@@ -8,6 +9,7 @@ AS $BODY$
 SELECT DISTINCT
 	course.course_id, course.department, course.course_number, course.course_name,
 	course_section.section_id, course_section.section_number, course_section.placeholder_professor_name,
+    section_assignment_preference.preference,
 	section_meeting.weekday, section_meeting.start_time, section_meeting.end_time, section_meeting.place
 FROM person_availability
 JOIN section_meeting ON
@@ -19,8 +21,7 @@ JOIN qualification ON
 qualified = 'true' AND
 qualification.course_id = (SELECT course_section.course_id FROM course_section WHERE course_section.section_id = section_meeting.section_id)
 JOIN section_assignment_preference ON
-section_assignment_preference.section_id = section_meeting.section_id AND
-section_assignment_preference.preference != 'Can''t Do'
+section_assignment_preference.section_id = section_meeting.section_id
 JOIN course ON
 course.course_id = qualification.course_id
 JOIN course_section ON
