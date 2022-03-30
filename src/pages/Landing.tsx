9d758fce372477ab8ed10ec01075f1e37dc7ff07
EvelynCrowe <evelyn.a.crowe@gmail.com>
2022-03-30 09:54:04 -0500

    added button

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import rick from '../assets/rick2.jpg.png'
+import './../Components/common.scss';
+/*<img id="landing" src={rick} alt=''/>*/
 
 export const Landing = () => {
   return (
-    <div className="Landing">
-      This is the landing page :D
+    <div className="app-container vstack">
+      <div className="landing-container2">
+        <button className="maroon button2">Start Scheduling</button>
+      </div>
     </div>
   );
 }
