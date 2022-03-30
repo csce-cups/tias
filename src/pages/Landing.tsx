198542d08b9d16d9e6a5dedc6c4da9877969d70a
EvelynCrowe <evelyn.a.crowe@gmail.com>
2022-03-30 10:30:18 -0500

    hardcoding bad

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -5,11 +5,9 @@ import './../Components/common.scss';
 
 export const Landing = () => {
   return (
-    <div className="background-image">
-      <div className="app-container vstack">
-        <div className="landing-container2">
-          <button className="maroon button2">Start Scheduling</button>
-        </div>
+    <div className="app-body vstack background-image">
+      <div className="landing-container2">
+        <button className="maroon button2">Start Scheduling</button>
       </div>
     </div>
   );
