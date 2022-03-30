079de35471d2aed945351983d3e9f16b9b2969d9
EvelynCrowe <evelyn.a.crowe@gmail.com>
2022-03-30 10:44:56 -0500

    done for now

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import rick from '../assets/rick2.jpg.png'
 import './../Components/common.scss';
-/*<img id="landing" src={rick} alt=''/>*/
 
 export const Landing = () => {
   return (
     <div className="app-body vstack background-image">
-      <div className="landing-container2">
+      <div className="landing-container">
         <button className="maroon button2">Start Scheduling</button>
       </div>
     </div>
