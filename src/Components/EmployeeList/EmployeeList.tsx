8a070cf69203c6ec82a4f32953e572ae7f58ca6a
Ben Beauchamp <benbeauchamp7@gmail.com>
2022-04-02 14:08:41 -0500

    The user may now specify what order the peer teachers are sorted in

--- src/Components/EmployeeList/EmployeeList.tsx
+++ src/Components/EmployeeList/EmployeeList.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { EmployeeRow } from './EmployeeRow'
 import { GenerateButton } from './GenerateButton'
 import { AcceptButton } from './AcceptButton'
@@ -10,17 +10,29 @@ interface Props {
 }
 
 export const EmployeeList: FC<Props> = () => {
+  const sortLastAlg = ((a: Person, b: Person) => a.last_name.localeCompare(b.last_name));
+  const sortFirstAlg = ((a: Person, b: Person) => a.first_name.localeCompare(b.first_name));;
+  const [sortLast, setSortLast] = useState<boolean>(true);
+
   return (
     <div className="vstack">
-      <div className="header">
-        <h2 className="slim">Employee</h2>
+      <div className="header" style={{marginBottom: '0'}}>
+        <h2 className="slim">Peer Teachers</h2>
+      </div>
+
+      <div className="sorter">
+        <div className="hstack">
+          <div className="element left">Sort By:</div>
+          <div className="element right" onClick={() => setSortLast(false)}>First Name</div>
+          <div className="element right" onClick={() => setSortLast(true)}>Last Name</div>
+        </div>
       </div>
 
       < contexts.employees.Consumer >
         {([employees, setEmployees]) => (
             (employees.length > 0)?
               <div className="scrollable">
-                {employees.map((e: Person, index: number) => (
+                {employees.sort((sortLast)? sortLastAlg : sortFirstAlg).map((e: Person, index: number) => (
                   < EmployeeRow employee={employees[index]} setEmployee={(e: Person) => {
                     employees[index] = e;
                     setEmployees(employees);
