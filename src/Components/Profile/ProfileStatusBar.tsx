a52f51207e3ebb6cb2d5f94ef44308dc69a2f43d
Ben Beauchamp <benbeauchamp7@gmail.com>
2022-04-03 11:43:07 -0500

    Added try catch for if parsing errors

--- src/Components/Profile/ProfileStatusBar.tsx
+++ src/Components/Profile/ProfileStatusBar.tsx
@@ -17,59 +17,63 @@ export const ProfileStatusBar = () => {
     const btn = document.getElementById('upload-schedule-button') as HTMLButtonElement;
     if (btn !== null) btn.innerHTML = 'Uploading...';
 
-    let icsdata = icsFileAsText.split("\n");
-    //ASSUMPTIONS:
-    //EVERY event has the same number of lines
-    //every invalid event is marked by the RRULE line having '=' at the end of the line
-    //the header is always 5 lines
-    let line = 5;
-    let dayLine = -1;
-    let startLine = -1;
-    let endLine = -1;
-    let len = 1;
-    let temp;
-    let r = false;
-    while ((temp = icsdata[line + len].split(":"))[0] !== "END") {
-      if (icsdata[line + len].charAt(icsdata[line + len].length - 1) === "\r") {
-        r = true;
+    try {
+      let icsdata = icsFileAsText.split("\n");
+      //ASSUMPTIONS:
+      //EVERY event has the same number of lines
+      //every invalid event is marked by the RRULE line having '=' at the end of the line
+      //the header is always 5 lines
+      let line = 5;
+      let dayLine = -1;
+      let startLine = -1;
+      let endLine = -1;
+      let len = 1;
+      let temp;
+      let r = false;
+      while ((temp = icsdata[line + len].split(":"))[0] !== "END") {
+        if (icsdata[line + len].charAt(icsdata[line + len].length - 1) === "\r") {
+          r = true;
+        }
+        if (temp[0] === "RRULE") {
+          dayLine = len;
+        } else if (temp[0] === "DTSTART") {
+          startLine = len;
+        } else if (temp[0] === "DTEND") {
+          endLine = len;
+        }
+        len++;
       }
-      if (temp[0] === "RRULE") {
-        dayLine = len;
-      } else if (temp[0] === "DTSTART") {
-        startLine = len;
-      } else if (temp[0] === "DTEND") {
-        endLine = len;
+      len++; //for the END line
+      if (r) {
+        icsdata = icsFileAsText.split("\r\n");
       }
-      len++;
-    }
-    len++; //for the END line
-    if (r) {
-      icsdata = icsFileAsText.split("\r\n");
-    }
-    let data: APIStudentUnavailability[] = [];
-    while (line < icsdata.length - 1) {
-      let byday = icsdata[line + dayLine];
-      let start = icsdata[line + startLine];
-      let end = icsdata[line + endLine];
-      let lastchr = byday.charAt(byday.length - 1);
-      if (lastchr !== "=") {
-        //then this is a good event
-        let days = byday.split("=");
-        let event: APIStudentUnavailability = {
-          DTSTART: start.split(":")[1],
-          DTEND: end.split(":")[1],
-          BYDAY: days[days.length - 1],
-        };
-        data.push(event);
+      let data: APIStudentUnavailability[] = [];
+      while (line < icsdata.length - 1) {
+        let byday = icsdata[line + dayLine];
+        let start = icsdata[line + startLine];
+        let end = icsdata[line + endLine];
+        let lastchr = byday.charAt(byday.length - 1);
+        if (lastchr !== "=") {
+          //then this is a good event
+          let days = byday.split("=");
+          let event: APIStudentUnavailability = {
+            DTSTART: start.split(":")[1],
+            DTEND: end.split(":")[1],
+            BYDAY: days[days.length - 1],
+          };
+          data.push(event);
+        }
+        line += len;
       }
-      line += len;
+  
+      API.saveUserUnavailability(data).then(() => {
+        if (btn !== null) btn.innerHTML = 'Upload Successful!';
+      }).catch(() => {
+        if (btn !== null) btn.innerHTML = 'An error occurred.';
+      })
+    } catch (TypeError) {
+      if (btn !== null) btn.innerHTML = 'An error occurred reading the file.';
     }
-
-    API.saveUserUnavailability(data).then(() => {
-      if (btn !== null) btn.innerHTML = 'Upload Successful!';
-    }).catch(() => {
-      if (btn !== null) btn.innerHTML = 'An error occurred.';
-    })
   };
 
   const readInputICSFile = (icsFile: any) => {
