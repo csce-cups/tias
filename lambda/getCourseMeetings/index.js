af626fa8bcb9acf9fcd646f73e155e976c631405
Shane-McGookey <ShaneMcGookey@Gmail.com>
2022-04-03 12:39:41 -0500

    Added appropriate error handling in getCourseMeetings and deprecated use of (most) parameter store values due to AWS free tier limitations.

--- lambda/getCourseMeetings/index.js
+++ lambda/getCourseMeetings/index.js
@@ -1,21 +1,31 @@
 const helper_functions = require("./helper_functions");
 
 exports.handler = async (event) => {
+    const response = {
+        "isBase64Encoded": false,
+        "statusCode": 200,
+        "headers": { "Content-Type": "application/json", 
+                     "Access-Control-Allow-Origin": "http://localhost:3000" }
+    };
+    
     const dbQuery = 
     `
         SELECT section_meeting.section_id, course.department, course.course_number, course_section.section_number, course_section.placeholder_professor_name,
                section_meeting.start_time, section_meeting.end_time, section_meeting.weekday, 
                section_meeting.place
-            FROM section_meeting
-            LEFT OUTER JOIN course_section ON course_section.section_id = section_meeting.section_id
-            LEFT OUTER JOIN course ON course.course_id = course_section.course_id
-            WHERE section_meeting.meeting_type = 'Laboratory'
-            ORDER BY section_meeting.weekday, section_meeting.start_time, (section_meeting.end_time - section_meeting.start_time),
-                     course.course_number, course_section.section_number
+        FROM section_meeting
+        LEFT OUTER JOIN course_section ON course_section.section_id = section_meeting.section_id
+        LEFT OUTER JOIN course ON course.course_id = course_section.course_id
+        WHERE section_meeting.meeting_type = 'Laboratory'
+        ORDER BY section_meeting.weekday, section_meeting.start_time, (section_meeting.end_time - section_meeting.start_time),
+                 course.course_number, course_section.section_number
     `;
     const params = [];
     
-    let dbRows = await helper_functions.queryDB(dbQuery, params);
+    let dbRows = await helper_functions.queryDB(dbQuery, params).catch((err) => {
+        helper_functions.GenerateErrorResponseAndLog(err, response, 'Unable to retrieve course meetings.');
+        return response;
+    });
     
     const responseBody = { };
     
@@ -28,13 +38,7 @@ exports.handler = async (event) => {
     }
 	
 	responseBody[dbRows[dbRows.length - 1].weekday] = dbRows.slice(prevSliceIndex, dbRows.length);
-    
-    const response = {
-        "isBase64Encoded": false,
-        "statusCode": 200,
-        "headers": { "Content-Type": "application/json", "Access-Control-Allow-Origin": "http://localhost:3000" },
-        "body": JSON.stringify(responseBody)
-    };
 
+    response.body = JSON.stringify(responseBody);
     return response;
 };
