300125c6c67fc79e2b790e7b8334545bc130abe2
Ben Beauchamp <benbeauchamp7@gmail.com>
2022-04-03 11:44:43 -0500

    Removed profile status bar

--- src/Components/Profile/ProfileStatusBar.tsx
+++ src/Components/Profile/ProfileStatusBar.tsx
@@ -107,13 +107,6 @@ export const ProfileStatusBar = () => {
     <div className="profile-status-bar">
       <input type="file" ref={fileInputRef} onChange={handleChange} style={{ display: "none" }}/>
       <button id="upload-schedule-button" className="blue button" onClick={handleClick}>Upload Schedule</button>
-      <div className="profile-status">
-        <div className="left element">Last Updated: </div>
-        <div className="fill element" />
-        <div className="element" style={{ marginRight: "5px" }}>
-          Jan 1, 1970
-        </div>
-      </div>
     </div>
   );
 }
