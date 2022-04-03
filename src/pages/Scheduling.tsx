2fa9506dc45cac6be80cd0e81c34b8322b75871b
Ben Beauchamp <benbeauchamp7@gmail.com>
2022-04-02 19:39:57 -0500

    Fixed an issue where sorting the employees during generation would mark everything as red

--- src/pages/Scheduling.tsx
+++ src/pages/Scheduling.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { EmployeeList } from '../Components/EmployeeList/EmployeeList';
 import { SchedulingWindow } from '../Components/Scheduling/SchedulingWindow';
 import '../Components/common.scss';
