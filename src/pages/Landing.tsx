c32b585c32b31514de07455176d92d877a6fe9b9
EvelynCrowe <evelyn.a.crowe@gmail.com>
2022-04-06 10:52:45 -0500

    added different contexts for central button

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import rick from '../assets/rick2.jpg.png'
 import './../Components/common.scss';
+import contexts from '../Components/APIContext';
 
 export const Landing = () => {
+  const user = useContext(contexts.user);
+  let text = "Sign In";
+  if(user.user?.administrator) {
+    text = "Start Scheduling";
+  } else if (user.user?.peer_teacher) {
+    text = "View Profile";
+  }
   return (
     <div className="app-body vstack background-image">
       <div className="landing-container">
-        <button className="maroon button2">Start Scheduling</button>
+        <button className="maroon button2">{text}</button>
       </div>
     </div>
   );
