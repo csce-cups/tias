73f7088327751606fed14e2ada59a5a9635e5e05
EvelynCrowe <evelyn.a.crowe@gmail.com>
2022-04-06 10:04:28 -0500

    centered video

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -4,7 +4,9 @@ import './../Components/common.scss';
 export const Tutorial = () => {
   return (
     <div className="app-body vstack">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>
+        <div className="landing-container">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>
+        </div>
     </div>
   );
 }
