5ba305e8cd8daa368fb281ce3c1209bd55d00824
pulchroxloom <code@dantebarbieri.dev>
2022-04-07 10:15:44 -0500

    Add routes by user role

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,20 +1,17 @@
 import React, { useContext } from 'react'
-import rick from '../assets/rick2.jpg.png'
 import './../Components/common.scss';
 import contexts from '../Components/APIContext';
+import { Link } from 'react-router-dom';
+import { GoogleButton } from '../Components/Misc/GoogleButton';
 
 export const Landing = () => {
   const user = useContext(contexts.user);
-  let text = "Sign In";
-  if(user.user?.administrator) {
-    text = "Start Scheduling";
-  } else if (user.user?.peer_teacher) {
-    text = "View Profile";
-  }
   return (
     <div className="app-body vstack background-image">
       <div className="landing-container">
-        <button className="maroon button2">{text}</button>
+        {user.user?.administrator ? <Link to='Scheduling' className="maroon button2" style={{"textDecoration": "none"}}>Start Scheduling</Link>
+        : user.user?.peer_teacher ? <Link to='Profile' className="maroon button2" style={{"textDecoration": "none"}}>View Profile</Link>
+        : < GoogleButton />}
       </div>
     </div>
   );
