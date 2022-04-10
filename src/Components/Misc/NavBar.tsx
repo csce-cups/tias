ea2e34167a171ddc28a333ef5e999c83b9204bfb
benbeauchamp7 <benbeauchamp7@gmail.com>
2022-04-10 12:43:49 -0500

    Lowercase routes

--- src/Components/Misc/NavBar.tsx
+++ src/Components/Misc/NavBar.tsx
@@ -9,10 +9,10 @@ export const NavBar = () => {
   return (
     <div className="navbar">
       <Link to='/' className="left element major" style={{textDecoration: 'none'}}>TIAS</Link>
-      {doShowScheduling && <Link to='Scheduling' className="left element">Scheduling</Link>}
-      {doShowAdmin && <Link to='Admin' className="left element">Admin</Link>}
-      {doShowProfile && <Link to='Profile' className="left element">Profile</Link>}
-      {doShowLabSwap && <Link to='Labswap' className="left element">LabSwap™</Link>}
+      {doShowScheduling && <Link to='scheduling' className="left element">Scheduling</Link>}
+      {doShowAdmin &&      <Link to='admin' className="left element">Admin</Link>}
+      {doShowProfile &&    <Link to='profile' className="left element">Profile</Link>}
+      {doShowLabSwap &&    <Link to='labswap' className="left element">LabSwap™</Link>}
       <div className="fill element"></div>
       < GoogleButton />
     </div>
