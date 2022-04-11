530b0f6be87ba1596c82345cf0aea047650f9b0d
pulchroxloom <code@dantebarbieri.dev>
2022-04-10 22:14:49 -0500

    Remove underlining

--- src/Components/Misc/NavBar.tsx
+++ src/Components/Misc/NavBar.tsx
@@ -9,10 +9,10 @@ export const NavBar = () => {
   return (
     <div className="navbar">
       <Link to='/' className="left element major" style={{textDecoration: 'none'}}>TIAS</Link>
-      {doShowScheduling && <Link to='scheduling' className="left element">Scheduling</Link>}
-      {doShowAdmin &&      <Link to='admin' className="left element">Admin</Link>}
-      {doShowProfile &&    <Link to='profile' className="left element">Profile</Link>}
-      {doShowLabSwap &&    <Link to='labswap' className="left element">LabSwap™</Link>}
+      {doShowScheduling && <Link to='scheduling' className="left element" style={{textDecoration: 'none'}}>Scheduling</Link>}
+      {doShowAdmin &&      <Link to='admin' className="left element" style={{textDecoration: 'none'}}>Admin</Link>}
+      {doShowProfile &&    <Link to='profile' className="left element" style={{textDecoration: 'none'}}>Profile</Link>}
+      {doShowLabSwap &&    <Link to='labswap' className="left element" style={{textDecoration: 'none'}}>LabSwap™</Link>}
       <div className="fill element"></div>
       < GoogleButton />
     </div>
