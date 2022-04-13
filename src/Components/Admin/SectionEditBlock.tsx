7f4fe9892d1dc90d82f3e901989101c0cbc24143
benbeauchamp7 <benbeauchamp7@gmail.com>
2022-04-13 17:44:41 -0500

    Saved and unsaved block states

--- src/Components/Admin/SectionEditBlock.tsx
+++ src/Components/Admin/SectionEditBlock.tsx
@@ -10,12 +10,17 @@ interface Props extends RenderBlockProps {
 }
 
 export const SectionEditBlock: FC<Props> = ({visible, size, inline, data}) => {
-  const [interacted, setInteracted] = useState<boolean>(false);
-  const [toUpdate, setToUpdate] = useContext(toUpdateContext);
   const {course_instance} = data;
   const [prof, setProf] = useState(course_instance.professor);
   const [desiredPTCount, setDesiredPTCount] = useState(course_instance.capacity_peer_teachers);
+  
+  const [interacted, setInteracted] = useState<boolean>(false);
+  const [toUpdate, setToUpdate, unsaved, setUnsaved] = useContext(toUpdateContext);
+  const [unsavedChanges, setUnsavedChanges] = useState(false);
+  const [saved, setSaved] = useState(false);
+
   const ref: any = useRef(null);
+
 
   const formatDate = (date: Date) => {
     const hour = date.getHours();
@@ -26,22 +31,31 @@ export const SectionEditBlock: FC<Props> = ({visible, size, inline, data}) => {
     return `${hour12}:${minutes} ${ampm}`;
   }
 
-  const addToUpdate = () => {
+  const registerUpdate = () => {
     const where = toUpdate.findIndex(({section_id}) => section_id === course_instance.section_id);
     if (where === -1) {
       toUpdate.push({
         section_id: course_instance.section_id,
         placeholder_proffessor_name: prof,
-        capacity_peer_teachers: desiredPTCount!
+        capacity_peer_teachers: desiredPTCount ?? 2
       })
     } else {
       toUpdate[where] = {
         section_id: course_instance.section_id,
         placeholder_proffessor_name: prof,
-        capacity_peer_teachers: desiredPTCount!
+        capacity_peer_teachers: desiredPTCount ?? 2
       }
     }
-    setToUpdate(toUpdate);
+    setToUpdate([...toUpdate]);
+    if (unsaved) setUnsaved(unsaved - 1);
+    setSaved(true);
+    setUnsavedChanges(false);
+  }
+
+  const handleChange = <T,>(e: React.ChangeEvent<HTMLInputElement>, setter: (arg: T) => void, setWith: T) => {
+    if (!unsaved) setUnsaved(unsaved + 1);
+    setter(setWith);
+    setUnsavedChanges(true);
   }
 
   let flex = calcFlex(visible, inline, size);
@@ -66,28 +80,49 @@ export const SectionEditBlock: FC<Props> = ({visible, size, inline, data}) => {
     };
   });
 
-  let color = { backgroundColor: 'red' };
-  color.backgroundColor = blockColors.get(course_instance.course_number)!
-  if (color.backgroundColor === undefined) console.error('Color not defined for:', JSON.stringify(course_instance))
+  let btnclass = (unsavedChanges)? 'cantdo' : (saved)? 'prefer' : 'indiff';
+  let background = {
+    background: (unsavedChanges)?
+      `repeating-linear-gradient(225deg,red,red 8px,black 8px,black 10px,transparent 10px,transparent)`
+      : (saved)?
+      `repeating-linear-gradient(225deg,green,green 8px,black 8px,black 10px,transparent 10px,transparent)`
+      : 
+      'transparent'
+  };
+
+  let title = (unsavedChanges)?
+    'This section has unsaved changes'
+    : (saved)?
+    'This section has changes that will be saved'
+    : 
+    `${course_instance.course_number}-${course_instance.section_number}`;
+  if (background.background === undefined) console.error('Color not defined for:', JSON.stringify(course_instance))
 
   return (
     <div ref={ref} className={`block ${interacted? 'interacted' : 'grow-h'}`} onClick={() => setInteracted(true)}
-      title={`${course_instance.course_number}-${course_instance.section_number}`} 
-      style={{...color, ...isVisible}}
+      title={title} 
+      style={{background: blockColors.get(course_instance.course_number)!, ...isVisible}}
     >
+      <div className="hat-container" style={{
+        ...background, 
+        height: '15px',
+        position: 'absolute',
+        border: '0',
+        zIndex: '-1'
+      }}/>  
       { interacted? 
         <div className="pref-pane">
           <div className="pref-pane-title">Edit {course_instance.department} {course_instance.course_number} section {course_instance.section_number}</div>
           <div className='ms5' style={{display: 'flex'}}>
             Desired Peer Teachers: &nbsp;
-            <input className="fill" type='number' value={desiredPTCount} onChange={e => setDesiredPTCount(+e.currentTarget.value)} style={{width: '3em'}}/>
+            <input className="fill" type='number' value={desiredPTCount} onChange={e => handleChange(e, setDesiredPTCount, +e.currentTarget.value)} style={{width: '3em'}}/>
           </div>
           <div className='ms5' style={{display: 'flex'}}>
             Professor: &nbsp;
-            <input className="fill" type='text' value={prof} onChange={e => setProf(e.currentTarget.value)}/>
+            <input className="fill" type='text' value={prof} onChange={e => handleChange(e, setProf, e.currentTarget.value)}/>
           </div>
           <div className="m5"/>
-          <button request-type="request" className="submit-button">Update section</button>
+          <button request-type="request" className={`submit-button ${btnclass}`} onClick={registerUpdate}>Update section</button>
         </div>
         :
         <>
--- src/Components/Admin/SectionEditButton.tsx
+++ src/Components/Admin/SectionEditButton.tsx
@@ -8,27 +8,58 @@ export interface EditableSection {
   capacity_peer_teachers: number
 }
 
-export const toUpdateContext = createContext<[EditableSection[], React.Dispatch<React.SetStateAction<EditableSection[]>>]>([
-  [], 0 as any
+export const toUpdateContext = createContext<[
+  EditableSection[], React.Dispatch<React.SetStateAction<EditableSection[]>>,
+  number, React.Dispatch<React.SetStateAction<number>>
+]>([
+  [], 0 as any,
+  0, 0 as any
 ]);
 
 export const SectionEditButton = () => {
-  const [editing, setEditing] = useState(false);
+  const [editing, setEditingState] = useState(false);
+  const [showRender, setShowRender] = useState(false);
+  const toUpdateState = useState<EditableSection[]>([]);
+  const unsavedState = useState<number>(0);
+
+  const setEditing = (to: boolean) => {
+    setEditingState(to);
+    if (!to) {
+      setTimeout(() => {
+        setShowRender(to);
+        toUpdateState[1]([]);
+        unsavedState[1](0);
+      }, 800);
+    } else {
+      setShowRender(to);
+    }
+  }
+
+  const submitUpdates = () => {
+    console.log(toUpdateState[0]);
+  }
 
   return (
     <>
       <div className={`full-modal-container interact-blocks ${editing? '' : 'hidden'}`}>
         <div className={`full-modal ${editing? '' : 'hidden'}`}>
-          < SchedulingWindow renderBlockType={SectionEditBlock} top={
-            <div className="hstack render-header">
-              <button className="green button fill" style={{marginLeft: '15px'}}>Save updated sections</button>
-              <button className="red button" onClick={() => setEditing(false)}>Leave without saving</button>
-            </div>
-          } options={{selectable: false}}/>
+          < toUpdateContext.Provider value={[...toUpdateState, ...unsavedState]} >
+            { showRender?
+              < SchedulingWindow renderBlockType={SectionEditBlock} top={
+                <div className="hstack render-header">
+                  <button className="green button fill" onClick={submitUpdates} style={{marginLeft: '15px'}}>
+                    Save{(toUpdateState[0].length > 0)? ` ${toUpdateState[0].length}` : ''} updated sections
+                  </button>
+                  <button className="red button" onClick={() => setEditing(false)}>Leave without saving</button>
+                </div>
+              } options={{selectable: false}}/>
+              : <></>
+            }
+          </ toUpdateContext.Provider >
         </div>
       </div>
       <div className="admin-changeover">
-        <button id="export-courses-button" className="purple button full" style={{marginBottom: '0'}} onClick={() => setEditing(true)}>Edit course sections</button>
+        <button className="purple button full" style={{marginBottom: '0'}} onClick={() => setEditing(true)}>Edit course sections</button>
       </div>
     </>
   )
