425e90c06f3d38caeb00876d9080dba276c06b78
Ben Beauchamp <benbeauchamp7@gmail.com>
2022-04-29 21:42:00 -0500

    PrefSelector and FormRow

--- src/Tests/Unit/Profile/ProfileFormRow.test.tsx
+++ src/Tests/Unit/Profile/ProfileFormRow.test.tsx
@@ -1,27 +1,45 @@
-import React, {useContext} from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import { ProfileFormRow } from '../../../Components/Profile/ProfileFormRow';
-import contexts from '../../../Components/APIContext';
-import API from '../../../modules/API';
-jest.mock('../../../modules/API');
-jest.mock('../../../Components/APIContext');
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import { ProfileFormRow } from "../../../Components/Profile/ProfileFormRow";
 
+jest.mock("../../../modules/API");
+jest.mock("../../../Components/APIContext");
 
-describe('ProfileFormRow', () => {
-    it('renders properly', () => {
-       render(<ProfileFormRow course_id={-1} course_name={"none"} qual={false} key={`pfrow-none`}/>); 
+describe("ProfileFormRow", () => {
+  it("shows the name", () => {
+    render(<ProfileFormRow course_id={-1} course_name={"coursename"} qual={false} />);
+    expect(screen.getByText(/coursename/i)).toBeInTheDocument();
+  });
+
+  it("states the current qualification (false)", () => {
+    render(<ProfileFormRow course_id={-1} course_name={"coursename"} qual={false} />);
+    expect(screen.getByText("Will not be scheduled for this course")).toBeInTheDocument();
+  });
+
+  it("states the current qualification (true)", () => {
+    render(<ProfileFormRow course_id={-1} course_name={"coursename"} qual={true} />);
+    expect(screen.getByText("Can be scheduled for this course")).toBeInTheDocument();
+  });
+
+  it("updates to true", () => {
+    render(<ProfileFormRow course_id={-1} course_name={"coursename"} qual={false}/>);
+
+    const select = screen.getByRole('combobox');
+    fireEvent.change(select, {
+      target: { value: true }
     });
 
-    it('updates properly', () => {
-        render(<ProfileFormRow course_id={-1} course_name={"none"} qual={false} key={`pfrow-none`}/>); 
-        const e = document.getElementById('${course_name}-prefs') as HTMLSelectElement;
-        e.onchange;
-        const select = screen.getByRole('select');
-        select.click();
-        fireEvent.change(select, {
-         //   target: {value: click}
-        })
-     });
+    expect(screen.getByText("Can be scheduled for this course")).toBeInTheDocument();
+  });
 
-   //still isn't testing update? 
-}); 
+  it("updates to false", () => {
+    render(<ProfileFormRow course_id={-1} course_name={"coursename"} qual={true}/>);
+
+    const select = screen.getByRole('combobox');
+    fireEvent.change(select, {
+      target: { value: false }
+    });
+
+    expect(screen.getByText("Will not be scheduled for this course")).toBeInTheDocument();
+  });
+});
